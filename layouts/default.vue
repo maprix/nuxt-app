87a76c8bccd9ae82c07fa1af98fbc2a890a36131
Marcus Pries <marcus.pries@web.de>
2021-08-18 14:32:14 +0200

    ssr enabled and server name set

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+     <div>
+      <v-app-bar color="deep-purple accent-4"
+        dense
+        dark
+      >
+        <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+        <v-toolbar-title>Test App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+     </v-app-bar>
+  </div>
+     <Nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+      <v-textarea v-model="input_text"></v-textarea>
+      <v-divider></v-divider>
+      <p>Text: {{ input_text }}</p>
+      </v-col>
+      <v-col>
+        Dies ist die zweite Spalte!
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      input_text: "Hello Text"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: left;
+}
+</style>
